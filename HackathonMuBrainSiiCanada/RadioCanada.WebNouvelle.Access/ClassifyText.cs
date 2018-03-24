6595730bd98de1a0540cfd40d74cda53878db3bc
RoyerRicaurte <royer.ricaurte@certicamara.com>
2018-03-24 10:12:07 -0400

    Add files via upload

---
+++ HackathonMuBrainSiiCanada/RadioCanada.WebNouvelle.Access/ClassifyText.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RadioCanada.WebNouvelle.Access
+{
+    public class ClassifyText
+    {
+        public ITextAnalyticsAPI Client {
+            get {
+                TextAnalyticsAPI clientN = new TextAnalyticsAPI() {  AzureRegion = AzureRegions.Westus, SubscriptionKey = "efc4cc9803844649a2e9af2fe0602afb" };
+                return clientN;
+            }
+            set {
+
+            }
+        }
+        public DetectedLanguage Lang { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public IList<string> KeyWords { get; set; }
+        public Dictionary <string,double?>Sentiments { get; set; }
+        public double Score { get; set; }
+
+        public classifyText(string text,string id)
+        {
+            Text = text;
+            Id = id;
+            getLanguage();
+
+        }
+
+        private void getLanguage()
+        {
+            
+            LanguageBatchResult result = Client.DetectLanguage(
+                    new BatchInput(
+                        new List<Input>()
+                        {
+                          new Input(Id, Text)
+                        }));
+            Lang = result.Documents[0].DetectedLanguages[0];
+            getKeyWords();
+        }
+        private void getKeyWords()
+        {
+            KeyPhraseBatchResult result2 = Client.KeyPhrases(
+                   new MultiLanguageBatchInput(
+                       new List<MultiLanguageInput>()
+                        {new MultiLanguageInput(Lang.Iso6391Name, Id,Text),}));
+            KeyWords = result2.Documents[0].KeyPhrases;
+            getSentiment();
+        }
+        private void getSentiment()
+        {
+            SentimentBatchResult result3 = Client.Sentiment(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                          new MultiLanguageInput(Lang.Iso6391Name, Id, Text)
+                        }));
+            Sentiments = new Dictionary<string, double?>();
+            foreach (var document in result3.Documents)
+            {
+                Sentiments.Add(document.Id, document.Score);
+            }
+            
+        }
+        public void InsertAnalyseInDB()
+        {
+            if (!RegistryExists(Id))
+            {
+                SqlConnection connection = new SqlConnection("Data Source=hackathonrc.database.windows.net;Initial Catalog=RadioCanadaNeuro;User ID=SiiCanadaAdmin;Password=S11C4n4d4Adm1n");
+                SqlCommand cmd = new SqlCommand();
+
+                //SqlDataReader reader;
+
+                List<string> arr = new List<string>();
+                foreach (var keyword in KeyWords)
+                {
+
+                    string sqlQuery = $"INSERT INTO Analyse (ArticleID,Keyword,Langue) Values({Id},'{keyword.Replace("'","''")}','{Lang.Iso6391Name}')";
+                    arr.Add(sqlQuery);
+                }
+                string allQueries = string.Join(";", arr);
+                cmd.CommandText = allQueries;
+                cmd.Connection = connection;
+
+                connection.Open();
+
+                int c = cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        private bool RegistryExists(string ArticleId) {
+            SqlConnection conn = new SqlConnection("Data Source=hackathonrc.database.windows.net;Initial Catalog=RadioCanadaNeuro;User ID=SiiCanadaAdmin;Password=S11C4n4d4Adm1n");
+            SqlCommand check = new SqlCommand("SELECT COUNT(Keyword) FROM [Analyse] WHERE ([ArticleID] = '"+ArticleId+"')", conn);
+            conn.Open();
+            int Exist = (int)check.ExecuteScalar();
+            conn.Close();
+            return(Exist > 0 ? true:false);
+        }
+    }
+
+}
---
+++ HackathonMuBrainSiiCanada/RadioCanada.WebNouvelle.Access/WebService.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioCanada.WebNouvelle.Access
+{
+
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+    public class WebService
+    {
+        public static string GetCategoryFrench(string body)
+        {
+            string txt = string.Empty;
+            string url = @"https://ussouthcentral.services.azureml.net/workspaces/cf61fcd5cd074cedab98530fbd2c10b8/services/429d9f50e5c84a75a106cdc61ad62396/execute?api-version=2.0&details=true";
+            const string apiKey = "VhICAn80PmO2ZqjURlqAmq6VokXaHScnp36siCVGnLWVe88Q5Tlm/dZ/r78ELaBbB5+jt0XKUd1eRlHpbmVMBg=="; // Replace this with the API key for the web service
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", "Bearer " + apiKey);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+           
+           
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            
+
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                //JObject json = JObject.Parse("test");
+                string stringData = "{\"Inputs\": {\"input1\": {\"ColumnNames\": [\"News\",\"Category\"],\"Values\": [[\""+body+"\",\"VALUE\"]]}},\"GlobalParameters\": {}}"; //place body here
+
+                streamWriter.Write(stringData);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)request.GetResponse();
+            var result = "";
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            
+
+            return result;
+        }
+
+        public static bool GetArticle(int NeuroId, out string Title, out string Body, out string CategoryName,out string ImageURL)
+        {
+            Body = "";
+            CategoryName = "";
+            Title = "";
+            ImageURL = "";
+
+            try
+            {
+                string txt = string.Empty;
+                string url = @"https://services.radio-canada.ca/hackathon/neuro/v1/news-stories/" + NeuroId.ToString() + "?client_key=bf9ac6d8-9ad8-4124-a63c-7b7bdf22a2ee";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    txt = reader.ReadToEnd();
+                }
+
+                JObject jObject = JObject.Parse(txt);
+                CategoryName = jObject["themeTag"]["name"].ToString();
+                Body = jObject["body"]["html"].ToString();
+                Title = jObject["title"].ToString();
+                try
+                {
+                    JObject joImageURL = (JObject)jObject["summaryMultimediaContentForDetail"];
+                    JArray joImageURL2 = (JArray)joImageURL["concreteImages"];
+                    var MetaImage = joImageURL2[0];
+                    
+                    ImageURL = MetaImage["mediaLink"]["href"].ToString();
+                }
+                catch
+                {
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+
+
+
+}
